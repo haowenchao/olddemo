196b90995499b01d78054494c743ad8730e54c50
haowenchao <haowenchao22@gmail.com>
2018-11-27 00:04:05 +0800

    Debug code of new scheduler
    The log is in minicom.cap

--- componment/thread.c
+++ componment/thread.c
@@ -88,6 +88,7 @@ void context_switch(void *from, void *to)
 	context_switch_flag = 1;
 
 	if(!context_irq) {
+		printf("##################################call swi\n\r");
 		context_irq = 0;
 		asm("swi 1");
 	}
@@ -124,7 +125,7 @@ struct wait_queue task_queue;
 
 DECLARE_WAIT_QUEUE_STATIC(t2_wait);
 
-static unsigned int cnt = 1;
+static volatile unsigned int cnt = 1;
 
 DECLARE_TASK(t1, 512, 20)
 {
@@ -136,6 +137,7 @@ DECLARE_TASK(t1, 512, 20)
 
 		delay_ms(500);
 		printf("task id = %d\n\r", current->pid);
+		printf("*************************cnt = %d\n\r", cnt);
 	}
 }
 
@@ -144,6 +146,7 @@ DECLARE_TASK(t2, 512, 20)
 	while(1) {
 		delay_ms(500);
 		printf("task id = %d\n\r", current->pid);
+		printf("*************************cnt = %d\n\r", cnt);
 
 		wait_event(&t2_wait, (cnt < 5));
 	}
@@ -154,6 +157,7 @@ DECLARE_TASK(t3, 512, 20)
 	while(1) {
 		delay_ms(500);
 		printf("task id = %d\n\r", current->pid);
+		printf("*************************cnt = %d\n\r", cnt);
 
 		if(cnt >= 10) {
 			cnt = 0;
