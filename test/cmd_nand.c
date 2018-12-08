6f710c4a670b4a368407c71502dd6bedc73f49cd
haowenchao <haowenchao22@gmail.com>
2018-12-08 18:37:26 +0800

    Add nand operation command

--- test/cmd_nand.c
+++ test/cmd_nand.c
@@ -6,7 +6,10 @@
  */
 
 #include "nand.h"
+#include "printf.h"
+#include "string.h"
 #include "s3c24xx.h"
+#include "componment/command.h"
 
 void write_nand(void)
 {
@@ -33,6 +36,25 @@ void read_nand(void)
 
 	for(i = 0; i < 4096; i++) {
 		uart_putc(buff2[i]);
+	}
+}
+
+DEFINE_COMMAND(nand, nand_opt)
+{
+	if(2 != argc) {
+		printf("\n\r\n\rusage: nand read to read nand\n\r");
+		printf("\n\r\n\rusage: nand write to write nand\n\r");
+
+		return;
+	}
+
+	if(!strcmp("read", argv[1])) {
+		read_nand();
+	}
+	else if(!strcmp("write", argv[1])) {
+		write_nand();
+	} else {
+		printf("\n\r\n\runsupport option: %s\n\r", argv[1]);
 	}
 }
 
